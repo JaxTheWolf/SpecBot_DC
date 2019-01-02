5b3b23b43ed3444ebbec50b0cfe4fbd5aa115e0e
JaxTheWolf <roman.lubij@gmail.com>
2019-01-02 17:32:11 +0100

    Removed old logger

--- commands/pc/credits.js
+++ commands/pc/credits.js
@@ -16,11 +16,6 @@ module.exports = class CreditsCommand extends Command {
     });
   }
   run(msg) {
-    function log() {
-      let path = require('path');
-      let filename = path.basename(__filename, `.js`);
-      console.log(`${filename} was used by ${msg.author.tag}.`);
-    }
 
     return msg.say(`SpecBot is coded by Roman Lubij, oko123#8509 and Designed by Jonne-Patrik Savimäki, The76i#1234, artwork by CrazyPenguin01#7682`);
     log(__filename, msg);
