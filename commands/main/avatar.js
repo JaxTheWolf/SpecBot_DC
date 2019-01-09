45cf005ca9881b0bec033982a9e8321266e362c3
Roman Lubij <roman.lubij@gmail.com>
2019-01-09 16:05:55 +0100

    Changed "user.avatarURL" to "user.displayAvatarURL" to avoid blank embed

--- commands/main/avatar.js
+++ commands/main/avatar.js
@@ -16,33 +16,31 @@ module.exports = class AvatarCommand extends Command {
       examples: [`avatar @oko123#8509`],
       args: [
         {
-          key: "user",
-          prompt: "Which user's avatar would you like to see?",
-          type: "user",
-          default: "",
-          error: "That isn't a valid user mention!"
+          key: `user`,
+          prompt: `Which user's avatar would you like to see?`,
+          type: `user`,
+          default: ``,
+          error: `That isn't a valid user mention!`
         }
       ]
     });
   }
   run(msg, { user }) {
-    try {
-      let embed = new RichEmbed();
+    let embed = new RichEmbed();
 
-      if (user === "") {
-        embed
-          .setTitle(`Here's your avatar:`)
-          .setImage(msg.author.avatarURL)
-          .setColor(rhc);
-        msg.say(embed);
-      } else {
-        embed
-          .setTitle(`Here's ${user.username}'s avatar:`)
-          .setImage(user.avatarURL)
-          .setColor(randomHexColor());
-        msg.say(embed);
-      }
-    } catch {}
+    if (user === ``) {
+      embed
+        .setTitle(`Here's your avatar:`)
+        .setImage(msg.author.displayAvatarURL)
+        .setColor(rhc);
+      msg.say(embed);
+    } else {
+      embed
+        .setTitle(`Here's ${user.username}'s avatar:`)
+        .setImage(user.displayAvatarURL)
+        .setColor(randomHexColor());
+      msg.say(embed);
+    }
     let toLog = `${path.basename(__filename, `.js`)} was used by ${
       msg.author.username
     }.`;
