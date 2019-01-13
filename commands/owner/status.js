9fbcd35f61548360945b28c18098d85aa473f06d
Roman Lubij <roman.lubij@gmail.com>
2019-01-13 14:59:35 +0100

    New command

---
+++ commands/owner/status.js
@@ -0,0 +1,41 @@
+const { Command } = require(`discord.js-commando`);
+const { options } = require(`../../options`);
+const log = require(`node-file-logger`);
+log.SetUserOptions(options);
+let path = require(`path`);
+
+module.exports = class StatusCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: `status`,
+      aliases: [`status`],
+      group: `owner`,
+      memberName: `status`,
+      description: `Shows the status of the \`bot\` systemd service Linux only`,
+      ownerOnly: true,
+      examples: [`status`]
+    });
+  }
+  run(msg) {
+    let shell = require(`shelljs`);
+
+    if (process.platform !== `win32`) {
+      shell.exec(`systemctl status bot`, { shell: `/bin/bash` }, function(
+        code,
+        stdout,
+        stderr
+      ) {
+        msg.say(stdout);
+      });
+    } else {
+      msg.say(`wip`);
+    }
+
+    let toLog = `${path.basename(__filename, `.js`)} was used by ${
+      msg.author.username
+    }.`;
+
+    console.log(toLog);
+    log.Info(toLog);
+  }
+};
