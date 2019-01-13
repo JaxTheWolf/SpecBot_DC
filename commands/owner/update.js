99364798255ced7a7a374bce8180e0e3c01805f4
Roman Lubij <roman.lubij@gmail.com>
2019-01-13 14:26:09 +0100

    Update

--- commands/owner/update.js
+++ commands/owner/update.js
@@ -27,6 +27,7 @@ module.exports = class UpdateCommand extends Command {
       );
     } else {
       shell.exec(`pwd`, function(code, stdout, stderr) {
+        console.log(`${code} ${stdout}, ${stderr}`);
         msg.say(stdout);
       });
     }
