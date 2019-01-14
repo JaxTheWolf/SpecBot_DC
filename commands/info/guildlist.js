dd9c21f841c82118ce6fca7e0ac4a8ae5db3dcb3
Roman Lubij <roman.lubij@gmail.com>
2019-01-14 19:24:29 +0100

    New command

---
+++ commands/info/guildlist.js
@@ -0,0 +1,35 @@
+const { Command } = require(`discord.js-commando`);
+const { options } = require(`../../options`);
+const { RichEmbed } = require(`discord.js`);
+const log = require(`node-file-logger`);
+log.SetUserOptions(options);
+let path = require(`path`);
+let randomHexColor = require(`random-hex-color`);
+
+module.exports = class GuildListCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: `guildlist`,
+      group: `info`,
+      memberName: `guildlist`,
+      description: `Lists all the guilds the bot is in.`,
+      examples: [`guildlist`]
+    });
+  }
+  run(msg) {
+    let guildlist = this.client.guilds.map(g => g.name).join(`\n`);
+    let embed = new RichEmbed()
+      .setColor(randomHexColor())
+      .setTitle(`Here's the list of guilds the bot is in:`)
+      .setDescription(guildlist);
+
+    msg.say(embed);
+
+    let toLog = `${path.basename(__filename, `.js`)} was used by ${
+      msg.author.username
+    }.`;
+
+    console.log(toLog);
+    log.Info(toLog);
+  }
+};
