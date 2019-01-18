06908914802f857b1a5f81214c57eb352b377882
Roman Lubij <roman.lubij@gmail.com>
2019-01-18 18:00:31 +0100

    pc1/2 can now be used to view your configuration without having to tag yourself

--- commands/pc/pc1.js
+++ commands/pc/pc1.js
@@ -18,7 +18,8 @@ module.exports = class PC1Command extends Command {
         {
           key: `user`,
           prompt: `Which user's configuration would you like to view?`,
-          type: `user`
+          type: `user`,
+          default: ``
         }
       ]
     });
@@ -27,20 +28,30 @@ module.exports = class PC1Command extends Command {
     const fs = require(`fs`);
     let readFrom = `${__dirname}/../../conf1`;
 
-    fs.readFile(`${readFrom}/${user.id}.txt`, `utf8`, function(err, data) {
-      if (err) {
-        msg.reply(`This person doesn't have a configuration yet!`);
-        console.log(err);
-      } else {
-        const embed = new RichEmbed()
-          .setTitle(`Here's ${user.username}'s configuration!`)
-          .setDescription(`${data}`)
-          .setColor(randomHexColor());
-        msg.channel.send({
-          embed
-        });
-      }
-    });
+    function retrievePC(user) {
+      fs.readFile(`${readFrom}/${user.id}.txt`, `utf8`, function(err, data) {
+        if (err) {
+          msg.reply(`This person doesn't have a configuration yet!`);
+          console.log(err);
+        } else {
+          const embed = new RichEmbed()
+            .setTitle(`Here's ${user.username}'s configuration!`)
+            .setAuthor(user.username, user.displayAvatarURL)
+            .setDescription(`${data}`)
+            .setColor(randomHexColor());
+          msg.channel.send({
+            embed
+          });
+        }
+      });
+    }
+
+    if (user === ``) {
+      retrievePC(msg.author);
+    } else {
+      retrievePC(user);
+    }
+
     let toLog = `${path.basename(__filename, `.js`)} was used by ${
       msg.author.username
     }.`;
