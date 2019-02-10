fbbb867f425fa28e065930c1bf8212d3d2394319
Roman Lubij <roman.lubij@gmail.com>
2019-02-10 20:09:04 +0100

    Fix. Oops

--- commands/pc/setpc1.js
+++ commands/pc/setpc1.js
@@ -1,5 +1,5 @@
 const { Command } = require(`discord.js-commando`)
-const { setConf } = require(`../../libs/setconf`)
+const { setConf } = require(`../../libs/setConf`)
 const { options } = require(`../../configs/options`)
 const log = require(`node-file-logger`)
 log.SetUserOptions(options)
