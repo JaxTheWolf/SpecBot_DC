5f29070639cba2f492eafb0d1ea7f1fd5d870c13
Roman Lubij <roman.lubij@gmail.com>
2019-02-27 14:54:00 +0100

    Revamped announce command Guilds will now be able to mute it and set a custom announcement channel

--- commands/owner/announce.js
+++ commands/owner/announce.js
@@ -24,31 +24,39 @@ module.exports = class AnnounceCommand extends Command {
     })
   }
   run (msg, { message }) {
-    try {
-      this.client.guilds.map(guild => {
-        let found = 0
+    log.Info(`${path.basename(__filename, `.js`)} was used by ${msg.author.username}.`)
+
+    this.client.guilds.map(guild => {
+      const channel = this.client.channels.get(this.client.provider.get(guild, `annchan`, null))
+
+      function isMuted (guild, client) {
+        const muted = client.provider.get(guild, `muteann`, null)
+        return !!(muted === false || muted === null)
+      }
+      function setDefAnnChannAndSendMessage (guild, client) {
+        let defaultChann
         guild.channels.map(c => {
-          if (found === 0) {
-            if (c.type === `text`) {
-              if (
-                c.permissionsFor(this.client.user).has(`VIEW_CHANNEL`) === true
-              ) {
-                if (
-                  c.permissionsFor(this.client.user).has(`SEND_MESSAGES`) ===
-                  true
-                ) {
-                  found = 1
-                  return c.send(message)
-                }
-              }
-            }
+          let found = 0
+          if (found === 0 &&
+            c.type === `text` &&
+            c.permissionsFor(client.user).has(`VIEW_CHANNEL`) === true &&
+            c.permissionsFor(client.user).has(`SEND_MESSAGES`) === true) {
+            found = 1
+            defaultChann = c.id
           }
         })
-      })
-    } catch (e) {
-      log.Info(`Could not send message to a (few) guild(s)!`)
-    }
+        return this.client.provider.set(guild, `annchan`, defaultChann)
+          .then(c => this.client.channels.get(c).send(message)
+            .then(msg => msg.channel.send(`This message was sent to a "random" channel. Please consider adding an announcement channel via -setannchannel channelID or #channel.`)))
+      }
 
-    log.Info(`${path.basename(__filename, `.js`)} was used by ${msg.author.username}.`)
+      if (isMuted(guild, this.client)) {
+        try {
+          channel.send(message)
+        } catch (e) {
+          setDefAnnChannAndSendMessage(guild, this.client)
+        }
+      }
+    })
   }
 }
