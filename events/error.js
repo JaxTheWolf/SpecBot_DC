c7cc43a3ac568e8c2cee4a9e7e6c026e60ed1e86
Roman Lubij <roman.lubij@gmail.com>
2019-03-09 19:38:12 +0100

    Removed spammy events

--- events/error.js
+++
@@ -1,7 +0,0 @@
-module.exports = (error) => {
-  const log = require(`node-file-logger`)
-  const { options } = require(`../configs/options`)
-  log.SetUserOptions(options)
-
-  log.Error(`A WebSocket error has emerged! Perish you bastard!`, `Event handlrs`, `nothing`, error)
-}
--- events/reconnecting.js
+++
@@ -1,7 +0,0 @@
-module.exports = () => {
-  const log = require(`node-file-logger`)
-  const { options } = require(`../configs/options`)
-  log.SetUserOptions(options)
-
-  log.Info(`Client's WebSocket is reconnecting!`)
-}
