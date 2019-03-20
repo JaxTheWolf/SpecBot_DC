e2abb22a3d5d86b620e5542c8e7040d3ad91f13a
Roman Lubij <roman.lubij@gmail.com>
2019-03-20 19:14:44 +0100

    Hopefully optimised the ready event

--- events/ready.js
+++ events/ready.js
@@ -5,14 +5,14 @@ module.exports = client => {
   const { setActivity } = require(`../libs/eventLibs`)
   log.SetUserOptions(options)
 
-  function createTable (table, tablename, args) {
-    table.prepare(`CREATE TABLE IF NOT EXISTS ${tablename}(${args});`).run()
+  function createTable (sql, tablename, args) {
+    sql.prepare(`CREATE TABLE IF NOT EXISTS ${tablename}(${args});`).run()
   }
 
   let sql = new SQLite(`./DBs/scores.sqlite3`)
   let table = sql.prepare(`SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'scores';`).get()
   if (!table[`count(*)`]) {
-    createTable(table, `scores`, `id TEXT PRIMARY KEY, user TEXT, guild TEXT, points INTEGER, level INTEGER`)
+    createTable(sql, `scores`, `id TEXT PRIMARY KEY, user TEXT, guild TEXT, points INTEGER, level INTEGER`)
     sql.prepare(`CREATE UNIQUE INDEX idx_scores_id ON scores (id);`).run()
     sql.pragma(`synchronous = 1`)
     sql.pragma(`journal_mode = wal`)
@@ -24,15 +24,15 @@ module.exports = client => {
   sql = new SQLite(`./DBs/configurations.sqlite3`)
   table = sql.prepare(`SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'conf1';`)
   if (!table[`count(*)`]) {
-    createTable(table, `conf1`, confArgs)
+    createTable(sql, `conf1`, confArgs)
   }
   table = sql.prepare(`SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'conf2';`)
   if (!table[`count(*)`]) {
-    createTable(table, `conf2`, confArgs)
+    createTable(sql, `conf2`, confArgs)
   }
   table = sql.prepare(`SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'server';`)
   if (!table[`count(*)`]) {
-    createTable(table, `server`, confArgs)
+    createTable(sql, `server`, confArgs)
   }
   setActivity(client)
 
