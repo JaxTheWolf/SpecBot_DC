f81f620e047e4892545029f9a122ede7c8c60a1b
Roman Lubij <roman.lubij@gmail.com>
2019-03-31 17:50:19 +0200

    General fixes Fixed embed command, ann command, setconf and setserver

--- commands/owner/announce.js
+++ commands/owner/announce.js
@@ -22,38 +22,39 @@ module.exports = class AnnounceCommand extends Command {
     })
   }
   run (msg, { message }) {
-    function isMuted (guild, client) {
-      const muted = client.provider.get(guild, `muteann`, null)
+    const cli = this.client
+    function isMuted (guild) {
+      const muted = cli.provider.get(guild, `muteann`, null)
       return !!(muted === null || false)
     }
 
-    function setDefAnnChannAndSendMessage (guild, client) {
+    function setDefAnnChannAndSendMessage (guild) {
       let defaultChann
       guild.channels.map(c => {
         let found = 0
         if (found === 0 &&
           c.type === `text` &&
-          c.permissionsFor(client.user).has(`VIEW_CHANNEL`) === true &&
-          c.permissionsFor(client.user).has(`SEND_MESSAGES`) === true) {
+          c.permissionsFor(cli.user).has(`VIEW_CHANNEL`) === true &&
+          c.permissionsFor(cli.user).has(`SEND_MESSAGES`) === true) {
           found = 1
           defaultChann = c.id
         }
       })
-      return this.client.provider.set(guild, `annchan`, defaultChann)
-        .then(c => this.client.channels.get(c).send(message)
-          .then(msg => msg.channel.send(`This message was sent to a "random" channel. Please consider adding an announcement channel via ${this.usage(`#channel or channelID`, msg.guild.commandPrefix)}.`)))
+      return cli.provider.set(guild, `annchan`, defaultChann)
+        .then(c => cli.channels.get(c).send(message)
+          .then(msg => msg.channel.send(`This message was sent to a "random" channel. Please consider adding an announcement channel via ${msg.guild.commandPrefix}annchannel set #channel or channelID.`)))
     }
     if (message === ``) {
       return sendCMDUsage(msg, this, `message`)
     } else {
-      this.client.guilds.map(guild => {
-        const channel = this.client.channels.get(this.client.provider.get(guild, `annchan`, null))
+      cli.guilds.map(guild => {
+        const channel = cli.channels.get(cli.provider.get(guild, `annchan`, null))
 
-        if (isMuted(guild, this.client)) {
+        if (isMuted(guild)) {
           try {
             channel.send(message)
           } catch (e) {
-            setDefAnnChannAndSendMessage(guild, this.client)
+            setDefAnnChannAndSendMessage(guild)
           }
         }
       })
--- commands/pc/setconf.js
+++ commands/pc/setconf.js
@@ -1,5 +1,4 @@
 const { Command } = require(`discord.js-commando`)
-const { sendCMDUsage } = require(`../../libs/miscLibs`)
 const { setConf } = require(`../../libs/pcLibs`)
 
 module.exports = class Setpc1Command extends Command {
@@ -16,95 +15,81 @@ module.exports = class Setpc1Command extends Command {
           prompt: `Which configuration would you like to set? (1 or 2)`,
           error: `Please enter a valid configuration (1 or 2)`,
           type: `string`,
-          default: ``,
           oneOf: [`1`, `2`]
         },
         {
-          default: ``,
           key: `CPU`,
           prompt: `What CPU do you have?`,
           type: `string`,
           wait: 90
         },
         {
-          default: ``,
           key: `GPU`,
           prompt: `What GPU do you have?`,
           type: `string`,
           wait: 90
         },
         {
-          default: ``,
           key: `RAM`,
           prompt: `What RAM do you use?`,
           type: `string`,
           wait: 90
         },
         {
-          default: ``,
           key: `CASE`,
           prompt: `Which CASE do you have?`,
           type: `string`,
           wait: 90
         },
         {
-          default: ``,
           key: `MOBO`,
           prompt: `What MOBO do you have?`,
           type: `string`,
           wait: 90
         },
         {
-          default: ``,
           key: `PSU`,
           prompt: `What PSU do you have?`,
           type: `string`,
           wait: 90
         },
         {
-          default: ``,
           key: `COOLER`,
           prompt: `What COOLER do you use?`,
           type: `string`,
           wait: 90
         },
         {
-          default: ``,
           key: `STORAGE`,
           prompt: `What STORAGE do you use?`,
           type: `string`,
           wait: 90
         },
         {
-          default: ``,
           key: `SCREEN`,
           prompt: `What SCREEN do you have?`,
           type: `string`,
           wait: 90
         },
         {
-          default: ``,
           key: `KEYBOARD`,
           prompt: `What KEYBOARD do you use?`,
           type: `string`,
           wait: 90
         },
         {
-          default: ``,
           key: `MOUSE`,
           prompt: `What MOUSE do you use?`,
           type: `string`,
           wait: 90
         },
         {
-          default: ``,
           key: `HEADSET`,
           prompt: `What HEADSET do you use?`,
           type: `string`,
           wait: 90
         },
         {
-          default: ``,
           key: `EXTRA`,
           prompt: `Do you have any extra accessories?`,
           type: `string`,
@@ -132,16 +117,12 @@ module.exports = class Setpc1Command extends Command {
       STORAGE
     }
   ) {
-    if (conf === ``) {
-      return sendCMDUsage(msg, this, [`configuration (1, 2 or server)`, `CPU`, `GPU`, `RAM`, `CASE`, `MOBO`, `PSU`, `COOLER`, `STORAGE`, `SCREEN`, `KEYBOARD`, `MOUSE`, `HEADSET`, `EXTRA`])
-    } else {
-      const content = `CPU: ${CPU}\nGPU: ${GPU}\nRAM: ${RAM}\nCASE: ${CASE}\nMOBO: ${MOBO}\nPSU: ${PSU}\nCOOLER: ${COOLER}\nSTORAGE: ${STORAGE}\nSCREEN: ${SCREEN}\nKEYBOARD: ${KEYBOARD}\nMOUSE: ${MOUSE}\nHEADSET: ${HEADSET}\nEXTRA: ${EXTRA}`
-      switch (conf) {
-      case `1`:
-        return setConf(msg, content, `conf1`, __dirname)
-      case `2`:
-        return setConf(msg, content, `conf2`, __dirname)
-      }
+    const content = `CPU: ${CPU}\nGPU: ${GPU}\nRAM: ${RAM}\nCASE: ${CASE}\nMOBO: ${MOBO}\nPSU: ${PSU}\nCOOLER: ${COOLER}\nSTORAGE: ${STORAGE}\nSCREEN: ${SCREEN}\nKEYBOARD: ${KEYBOARD}\nMOUSE: ${MOUSE}\nHEADSET: ${HEADSET}\nEXTRA: ${EXTRA}`
+    switch (conf) {
+    case `1`:
+      return setConf(msg, content, `conf1`, __dirname)
+    case `2`:
+      return setConf(msg, content, `conf2`, __dirname)
     }
   }
 }
